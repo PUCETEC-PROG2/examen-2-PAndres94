74591f91c60e34b34d02c827525aaf1a1e333de2
Andres040903 <eacaillagua@puce.edu.ec>
2024-07-13 13:14:48 -0500

    parcial2

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# En el archivo views.py de la aplicación movies
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    return render(request, 'movies/index.html', {'movies': movies})
+
+def movie_detail(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    return render(request, 'movies/movie_detail.html', {'movie': movie})
